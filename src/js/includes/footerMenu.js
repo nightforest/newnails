60dbdc769cd151a3cf477fba4cbccf65831dd3c3
Oleksandr Karachalov <alexandr12ua@gmail.com>
2024-06-19 12:39:52 +0200

    fix google speed

--- src/js/includes/footerMenu.js
+++ src/js/includes/footerMenu.js
@@ -2,6 +2,9 @@ const footerMenu = () => {
     const changeAttr = () => {
         const footerMenuToggleAll = document.querySelectorAll('.footer-menu__toggle');
         const footerMenuAll = document.querySelectorAll('.footer-menu__inner');
+        const footerMenuLinksAll = document.querySelectorAll('.footer-menu__link');
+        const searchToggle = document.querySelector('.search-toggle');
+        const search = document.querySelector('.search');
 
         footerMenuToggleAll?.forEach(item => {
             item.setAttribute('aria-expanded', false);
@@ -10,6 +13,13 @@ const footerMenu = () => {
         footerMenuAll?.forEach(item => {
             item.setAttribute('aria-hidden', true);
         });
+
+        footerMenuLinksAll?.forEach(item => {
+            item.setAttribute('tabindex', '-1');
+        });
+
+        searchToggle?.setAttribute('aria-expanded', false);
+        search?.setAttribute('aria-hidden', true);
     };
 
     if (window.innerWidth < 768) {
